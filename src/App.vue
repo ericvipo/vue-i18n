c1aecd93ffd651884ab6383e282fc2d869e47978
Eric <ericvpoma@gmail.com>
2023-05-16 21:25:39 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloI18n />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import HelloI18n from "./components/HelloI18n.vue";
+
+export default {
+  name: "App",
+  components: {
+    // HelloWorld,
+    HelloI18n,
+  },
+  created() {
+    const locale = localStorage.getItem('locale')
+
+    if(locale) {
+      this.$root.$i18n.locale = locale
+    } else if(navigator.language) {
+      console.log('spanish rules!');
+      this.$i18n.locale = navigator.language.substring(0, 2);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloI18n.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <!-- <p>{{ $t("hello", {name}) }}</p>
+    <p>{{ $t("email", {email}) }}</p>
+    <p>{{ $t("goodbye") }}</p> -->
+
+    <p>{{ $tc("car", 2) }}</p>
+    <p>{{ $tc("person", 0) }}</p>
+    <p>{{ $tc("person", 1) }}</p>
+    <p>{{ $tc("person", 9) }}</p>
+
+
+    <button class="button" @click="setLocale('en')">English</button>
+    <button class="button" @click="setLocale('es')">Spanish</button>
+    <button class="button" @click="setLocale('de')">German</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloI18n",
+  data() {
+    return {
+      name: 'Eric',
+      email: 'ericvpoma@gmail.com'
+    }
+  },
+  methods: {
+    setLocale(locale) {
+      this.$root.$i18n.locale = locale;
+      // localStorage.setItem('locale', locale)
+    }
+  }
+};
+</script>
+
+<!-- <i18n>
+{
+  "en": {
+    "hello": "Hello i18n in SFC!"
+  },
+  "es": {
+    "hello": "Hola i18n in SFC!"
+  }
+}
+</i18n> -->
